d1d1d360932818b94a8b2bc998eff374ff9e041c
Ting-Chi Wong <tcwong007@gmail.com>
2021-05-22 12:46:04 +0200

    iteration 2 done

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -4,7 +4,32 @@ axios({
 })
   .then((response) => {
     console.log(response.data);
+    printTheChart(response.data);
   })
   .catch((err) => {
     console.log("oops", err);
   });
+
+  function printTheChart(stockData) {
+    console.log("hello");
+    const dailyData = stockData['bpi'];
+   
+    const stockDates = Object.keys(dailyData);
+    const stockPrices = stockDates.map(date => dailyData[date]['4. close']);
+   
+    const ctx = document.getElementById('my-chart').getContext('2d');
+    const chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: stockDates,
+        datasets: [
+          {
+            label: 'Stock Chart',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: stockPrices
+          }
+        ]
+      }
+    }); // closes chart = new Chart()
+  } // closes printTheChart()
